4f54671a518b30eebdfcb406c72eb06c2a01ce1c
MsVivianH <hendersonv022@gmail.com>
2019-09-19 16:03:10 -0500

    Hello World

---
+++ src/com/company/FizzArray2.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class FizzArray2 {
+
+    public static int fizzarray() {
+
+        public int[] fizzArray ( int n){
+            int[] result = new int[n];
+            for (int i = 0; i < n; i++)
+            result[i] = i; 5
+            return result;
+        }
+    }
+}
+
+snacks[chips, dip, cookies, 4G]
+
+
+numbers[2, 5, 23, 16]
+
+0
+0, 1, 2 3
+
